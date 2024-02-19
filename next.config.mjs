76a175c7eb9a638853d47e00800727671a484135
aldodwrzy <aldodwrzy@gmail.com>
2024-02-19 16:56:47 +0700

    Update nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+};
+
+module.exports = {
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+    ...nextConfig
+  }
 
 export default nextConfig;
